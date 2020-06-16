c93ff0318eed01d14f900318c60a249cbf5760b0
Sarah <sarah.akus@gmail.com>
2020-06-16 12:05:06 -0400

    add app

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # Project-specific
     'users',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,4 @@ INTERNAL_IPS = [
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
